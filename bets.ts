cd4a8d356637ea510ab388ec8897e811cef3851e
brbala10 <161604574+brbala10@users.noreply.github.com>
2024-10-28 22:04:16 -0300

    Bets 90% concluída
    Ainda tem que trocar os nomes que não condizem com as outras funções e mudar o lugar da função que tira o dinheiro da pessoa.

--- bets.ts
+++ bets.ts
@@ -1,12 +1,61 @@
 import {Request, RequestHandler, Response} from "express";
 import OracleDB, { oracleClientVersion } from "oracledb";
-import { FinancialHandler } from "../financial/financial";
+import { FinancialHandler } from "./financial/financial";
+import { EventsHandler } from "./events/events";
+
 
 export namespace BetsHandler {
     {
         export type Bet = {
-            userID: string;
+            CPF: string;
             amount: number
+            eventId: string;
+            option: number;
         }
+    }
+
+    export async function betOnEvent(req: Request, res: Response) {
+        const { CPF, amount, eventId } = req.body;
+        
+
+        const event = EventsHandler.findEventById(eventId);
+        if (!event) {
+            return res.status(400).json({ message: "Evento inválido." });
+        }
+
+        const Balance = await FinancialHandler.getBalance(CPF);
+        if (Balance < amount) {
+            return res.status(400).json({ message: "Saldo insuficiente." });
+        }
+
+        const newBalance = await FinancialHandler.withdrawFunds(CPF, amount);
+
+        const connection = await OracleDB.getConnection();
+        try {
+            await connection.execute(
+                'INSERT INTO Bets (CPF, eventId, amount, option) VALUES (:CPF, :eventId, :amount, :option)',
+                { CPF, eventId, amount },
+                { autoCommit: true }
+            );
+            res.status(200).json({ message: "Aposta registrada!", event: event.name })
+        }   catch (error) {
+                await connection.rollback();
+                res.status(500).json({ message: "Erro ao registrar a aposta." });
+        } finally {
+            await connection.close();
+        }
+
+        if (event.statusEvent === option) {
+            const winnings = amount * event.oddsMultiplier; 
+            await FinancialHandler.addFunds(CPF, winnings);
+            res.status(200).json({
+                message: "Você ganhou a aposta!",
+                winnings: winnings,
+                finalBalance: Balance + winnings
+            });
+        } else {
+            res.status(200).json({ message: "Você perdeu a aposta!" });
+        }
+    
     }
 }
